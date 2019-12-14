db14432b096fab4fa4c792733cc2c21afa9da68e
feng <908087098@qq.com>
2019-12-14 22:14:12 +0800

    Engine -> View

--- src/polyfill/View.ts
+++ src/polyfill/View.ts
@@ -1,7 +1,7 @@
 namespace feng3d
 {
     // 默认在 Scene.init 添加物理世界模块
-    functionwrap.extendFunction(feng3d.Engine, "createNewScene", function (r)
+    functionwrap.extendFunction(feng3d.View, "createNewScene", function (r)
     {
         r.gameObject.addComponent(CANNON.PhysicsWorld);
         return r;
